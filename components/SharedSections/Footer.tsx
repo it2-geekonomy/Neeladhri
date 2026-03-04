252c12bb87d201ffffe3a314737c97838bb38081
Srivalli-Yaarlagadda <srivalli@geekonomy.in>
2026-03-04 11:19:43 +0530

    Added Favicon

---
+++ components/SharedSections/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="w-full p-4 bg-gray-100 text-center">
+      Footer
+    </footer>
+  );
+}
---
+++ components/SharedSections/Navbar.tsx
@@ -0,0 +1,7 @@
+export default function Navbar() {
+  return (
+    <nav className="w-full p-4 bg-white shadow text-center">
+      Navbar
+    </nav>
+  );
+}
