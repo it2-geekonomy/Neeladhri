ef0a7fd8563b40ae0f926f5d211000c055d754bb
Srivalli-Yaarlagadda <srivalli@geekonomy.in>
2026-04-27 11:42:50 +0530

    updated routing

---
+++ app/contact/page.tsx
@@ -0,0 +1,11 @@
+import ContactBanner from "@/components/Sections/ContactBanner";
+import ContactSection from "@/components/Sections/ContactSection";
+
+export default function ContactPage() {
+  return (
+    <>
+      <ContactBanner />
+      <ContactSection />
+    </>
+  );
+}
