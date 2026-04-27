a804818ee8c86524a968eaacaccc56194897a352
Srivalli-Yaarlagadda <srivalli@geekonomy.in>
2026-04-27 11:16:47 +0530

    updated about routing

---
+++ app/about/page.tsx
@@ -0,0 +1,11 @@
+import Typography from "@/lib/Typography";
+
+export default function AboutPage() {
+  return (
+    <section className="min-h-screen flex items-center justify-center">
+      <Typography variant="h2">
+        About page
+      </Typography>
+    </section>
+  );
+}
