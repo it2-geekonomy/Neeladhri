4606a9e12c7c471af7dd027ce7dccd8e2967caae
Srivalli-Yaarlagadda <srivalli@geekonomy.in>
2026-04-27 12:00:44 +0530

    updated routing

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -6,8 +6,8 @@ export default function BlogPage() {
   return (
     <>
       <BlogsSection />
-      <ContemporaryLiving />
       <FlooringIdeasSection />
+      <ContemporaryLiving />
     </>
   );
 }
